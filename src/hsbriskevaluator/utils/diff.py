764923489138cf9ede4464cc14cfb724e1e85b99
A7um <lgcpku@gmail.com>
2025-07-22 18:18:36 +0800

    feat: add detailed url and dir to repo diff result

--- src/hsbriskevaluator/utils/diff.py
+++ src/hsbriskevaluator/utils/diff.py
@@ -6,22 +6,39 @@ from .file import is_binary, detect_language, get_data_dir
 import subprocess
 from .apt_utils import AptUtils
 from ..collector.github_collector import GitHubRepoCollector
+
 logger = logging.getLogger(__name__)
+
 
 class DiffFile(BaseModel):
     name: str = Field(description="Name of the file")
     similarity: float = Field(description="Similarity rate of the file (0-1)")
 
+
 class DiffResult(BaseModel):
-    same_project: bool = Field(description="Whether the files are from the same project")
-    files : List[DiffFile] = Field(description="List of files with their similarity rates")
+    same_project: bool = Field(
+        description="Whether the files are from the same project"
+    )
+    dir1: str
+    dir2: str
+    url1: str
+    url2: str
+    files: List[DiffFile] = Field(
+        description="List of files with their similarity rates"
+    )
+
 
 class Comparator:
-    def __init__(self,github_collector: GitHubRepoCollector=GitHubRepoCollector(), apt_utils: AptUtils=AptUtils()):
+    def __init__(
+        self,
+        github_collector: GitHubRepoCollector = GitHubRepoCollector(),
+        apt_utils: AptUtils = AptUtils(),
+    ):
         self.github_collector = github_collector
         self.apt_utils = apt_utils
         pass
-    def compare_dirs(self, dir1: str, dir2: str) -> DiffResult: 
+
+    def compare_dirs(self, dir1: str, dir2: str, url1: str, url2: str) -> DiffResult:
         detector = CopyDetector(test_dirs=[dir1], ref_dirs=[dir2])
         detector.run()
         similar_list = detector.get_copied_code_list()
@@ -35,24 +52,35 @@ class Comparator:
                 continue
             if detect_language(file) in ("Groff", "Text only", "Unknown"):
                 continue
-            similarity = next((x for x in similar_list if x[2] == file), [0,])[0]
+            similarity = next(
+                (x for x in similar_list if x[2] == file),
+                [
+                    0,
+                ],
+            )[0]
             files.append(DiffFile(name=file, similarity=similarity))
         files = sorted(files, key=lambda file: file.similarity, reverse=True)
         return DiffResult(
-            same_project=(files[len(files)*4//5].similarity >= 0.8),
-            files=files
+            same_project=(files[len(files) * 4 // 5].similarity >= 0.8),
+            files=files,
+            url1=url1,
+            url2=url2,
+            dir1=dir1,
+            dir2=dir2,
         )
+
     def _clone_repo(self, repo_url: str, dest_dir: str) -> str:
-        data_dir = get_data_dir() / "diff" / dest_dir 
+        data_dir = get_data_dir() / "diff" / dest_dir
         if data_dir.exists():
             import shutil
+
             shutil.rmtree(data_dir)
         subprocess.run(["git", "clone", repo_url, str(data_dir)], check=True)
         return str(data_dir)
+
     def clone_and_compare(self, package: str) -> DiffResult:
         github_url = self.github_collector.search_repo(package).clone_url
         debian_url = self.apt_utils.get_package_git(package)
         github_path = self._clone_repo(github_url, f"{package}/github")
         debian_path = self._clone_repo(debian_url, f"{package}/debian")
-        return self.compare_dirs(debian_path, github_path)
-
+        return self.compare_dirs(debian_path, github_path, debian_url, github_url)
