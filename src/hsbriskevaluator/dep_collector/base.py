e5b8d5591d55d11e77536f55533c2c8121760046
A7um <lgcpku@gmail.com>
2025-09-03 15:35:52 +0800

    chore: add base class

---
+++ src/hsbriskevaluator/dep_collector/base.py
@@ -0,0 +1,10 @@
+"""
+collect dependencies recursively for a specific package
+"""
+class BaseDepCollector():
+    plateform: str 
+    def __init__(self):
+        pass
+
+    def collect(self, package_name:str):
+        pass
